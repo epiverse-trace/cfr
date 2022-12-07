b875b47d37843555a2ac0491f9164e8a3740f8b1
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-12-07 15:09:32 +0000

    Clean static_cfr + exs, output is named vector

--- R/static_cfr.R
+++ R/static_cfr.R
@@ -1,107 +1,112 @@
-#' Calculates the CFR at a single point in time using in the case and death time
-#' series supplied. Uses all of the time points supplied by the user and returns
-#' an estimate for a single point in time
+#' Static case fatality rate
+#'
+#' @description Calculates the CFR at a single point in time using in the case
+#' and death time series supplied. Uses all of the time points supplied by the
+#' user and returns an estimate for a single point in time.
 #'
 #' @param df_in A data.frame containing the outbreak data. A daily time series
-#' with dates or some other absolute indicator of time (e.g. epiday/epiweek) and 
+#' with dates or some other absolute indicator of time (e.g. epiday/epiweek) and
 #' the numbers of new cases and new deaths at each time point
-#' 
+#'
 #' @param correct_for_delays A boolean flag indicating whether the user wishes
 #' to correct for the delay between case detection and death. FALSE corresponds
 #' to a naive CFR being calculated, TRUE corresponds to the user calculating a
 #' corrected CFR
-#' 
-#' @param delay_pmf The delay distribution used, in the form of a probability 
+#'
+#' @param delay_pmf The delay distribution used, in the form of a probability
 #' mass function parameterised by time. I.e. f(t) which gives the probability a
 #' case has a known outcomes (i.e. death) at time t, parameterised with
-#' disease-specific parameters before it is supplied here. A typical example 
+#' disease-specific parameters before it is supplied here. A typical example
 #' would be a symptom onset to death delay distribution
+#' @param poisson_threshold The case count above which to use Poisson
+#' approximation. Set to 200 by default.
 #'
-#' @return A data.frame containing the MLE and 95% confidence interval of the
-#' CFR estimates
+#' @return A named vector with the MLE and 95% confidence interval of the
+#' CFR estimates, named "cfr_me", "cfr_low", and "cfr_high".
 #' @export
 #'
 #' @examples
-#' library(lubridate)
-#' library(epiparameter)
-#' 
-#' # read in the dataset in question. 
-#' # dataset needs 
-#' df_ebola <- read.csv("data/ebola_1976.csv")
-#' 
-#' df_ebola$date <- ymd(df_ebola$date)
-#' 
-#' # read in onset to death distribution
-#' onset_to_death_ebola <- epidist("ebola","onset_to_death")$pmf
-#' 
-#' calculate static naive and corrected CFRs
-#' ncfr <- calcalate_static_cfr(df_ebola, correct_for_delays = FALSE)
-#' ccfr <- calcalate_static_cfr(df_ebola, correct_for_delays = TRUE, onset_to_death_ebola)
-#' 
-#' # just a function that formats the output of the CFR data.frames 
-#' # nicely and prints to the terminal
-#' format_cfr_output_neatly(ncfr)
-#' format_cfr_output_neatly(ccfr)
+#' # Get onset to death distribution from epiparameter
+#' onset_to_death_ebola <- epiparameter::epidist("ebola", "onset_to_death")$pmf
+#'
+#' # load stored ebola case data from 1976 outbreak
+#' data("ebola1976")
+#'
+#' # Calculate static naive CFR using saved
+#' static_cfr(df_in = ebola1976, correct_for_delays = FALSE)
+#'
+#' # Calculate static corrected CFRs
+#' static_cfr(
+#'   df_in = ebola1976,
+#'   correct_for_delays = TRUE,
+#'   delay_pmf = onset_to_death_ebola
+#' )
 static_cfr <- function(df_in,
                        correct_for_delays = TRUE,
-                       delay_pmf) {
-  
+                       delay_pmf,
+                       poisson_threshold = 200) {
+
   # returns error message if no delay distribution is supplied, but correction
   # for delays was requested
-  if(correct_for_delays == TRUE & missing(delay_pmf)) {
-    stop("To correct for the delay between case detection and death,
-  please specify an onset-to-death (or similar) probability mass function")
-    
+  if (missing(delay_pmf) && (correct_for_delays)) {
+    stop(
+      "To correct for the delay between case detection and death,\
+       please specify an onset-to-death (or similar) probability mass function"
+    )
   }
-  
+  stopifnot(
+    "Case data must contain columns `cases` and `deaths`" =
+      (all(c("cases", "deaths") %in% colnames(df_in)))
+  )
+
   # calculating the naive CFR: (total deaths)/(total cases)
-  # with the standard binomial 
-  if(correct_for_delays == FALSE) {
-    
+  # with the standard binomial
+  if (!correct_for_delays) {
+
     # calculating the total number of cases (without correecting) and deaths
     total_cases <- sum(df_in$cases)
     total_deaths <- sum(df_in$deaths)
-    
+
     # calculating the central estimate
-    cfr_me <- total_deaths/total_cases
-    
+    cfr_me <- total_deaths / total_cases
+
     # calculating the lower and upper 95% confidence interval using the exact
     # binomial test
-    cfr_conf <- binom.test(round(total_deaths), total_cases, p = 1)
-    
+    cfr_conf <- stats::binom.test(round(total_deaths), total_cases, p = 1)
+
     # extracting the lower and upper intervals respectively
-    cfr_lo <- cfr_conf$conf.int[1]
-    cfr_hi <- cfr_conf$conf.int[2]
-    
-    # putting together the estimates 
-    cfr_estimate <- data.frame(cfr_me = cfr_me, 
-                               cfr_lo = cfr_lo,
-                               cfr_hi = cfr_hi)
-  }
-  
-  # calculating the corrected cfr, corrected for delay between case detection 
-  # and outcome
-  if(correct_for_delays == TRUE) {
-    
+    cfr_lims <- cfr_conf$conf.int
+
+    # putting together the estimates
+    cfr_estimate <- c(cfr_me, cfr_lims)
+    names(cfr_estimate) <- c("cfr_me", "cfr_low", "cfr_high")
+  } else {
+    # calculating the corrected cfr, corrected for delay between case detection
+    # and outcome
     # calculating the number of cases with known outcome, used as a replacement
     # for total deaths in the original cfr formula
-    df_corrected <- known_outcomes(df_in,
-                                   delay_pmf,
-                                   cumulative = FALSE)
-    
+    df_corrected <- known_outcomes(
+      df_in = df_in,
+      delay_pmf = delay_pmf,
+      cumulative = FALSE
+    )
+
     # calculating the total number of cases and deaths after correcting for
     # the number of cases with known outcomes and using this estimate as the
     # of deaths
     total_cases <- sum(df_corrected$cases)
     total_deaths <- sum(df_corrected$deaths)
-    
+
     # calculating the proportion of cases with known outcome
-    u_t <- sum(df_corrected$known_outcomes)/total_cases
-    
+    u_t <- sum(df_corrected$known_outcomes) / total_cases
+
     # calculating the maximum likelihood estimate and 95% confidence interval
     # using the binomial likelihood function from Nishiura
-    cfr_estimate <- ccfr_uncertainty(total_cases, total_deaths, u_t)
+    cfr_estimate <- ccfr_uncertainty(
+      total_cases = total_cases, total_deaths = total_deaths, u_t = u_t,
+      poisson_threshold = poisson_threshold
+    )
   }
-  
-  return(cfr_estimate)
+
+  cfr_estimate
 }
