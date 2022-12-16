374838313db9e6bf26cda702d5daa82a6b7e968d
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-12-16 15:03:31 +0000

    Rewrite `rolling_cfr` to avoid repeated `static_cfr` calls

--- R/rolling_cfr.R
+++ R/rolling_cfr.R
@@ -21,6 +21,9 @@
 #' disease-specific parameters before it is supplied here. A typical example
 #' would be a symptom onset to death delay distribution
 #'
+#' @param poisson_threshold The case count above which to use Poisson
+#' approximation. Set to 200 by default.
+#'
 #' @return A data.frame containing the same case and death time series supplied
 #' with the desired CFR (naive or corrected) appended at each time point
 #' as extra columns
@@ -28,7 +31,8 @@
 #'
 #' @examples
 #' # Get onset to death distribution from epiparameter
-#' onset_to_death_ebola <- epiparameter::epidist("ebola", "onset_to_death")$pmf
+#' onset_to_death_ebola <- epiparameter::epidist("ebola", "onset_to_death")
+#' delay_pmf <- onset_to_death_ebola$pmf
 #'
 #' # Calculate rolling naive CFR
 #' df_ncfr <- rolling_cfr(ebola1976, correct_for_delays = FALSE)
@@ -37,11 +41,12 @@
 #' df_ccfr <- rolling_cfr(
 #'   df_in = ebola1976,
 #'   correct_for_delays = TRUE,
-#'   onset_to_death_ebola
+#'   delay_pmf
 #' )
 rolling_cfr <- function(df_in,
                         correct_for_delays = TRUE,
-                        delay_pmf) {
+                        delay_pmf,
+                        poisson_threshold = 200) {
 
   # returns error message if no delay distribution is supplied, but correction
   # for delays was requested
@@ -62,31 +67,56 @@ rolling_cfr <- function(df_in,
       (all(c("cases", "deaths") %in% colnames(df_in)))
   )
 
-  # calculating the number of days in the time series to be looped over
-  n_days <- seq_len(nrow(df_in))
+  if (correct_for_delays) {
+    # calculating the total number of cases and deaths after correcting for
+    # the number of cases with known outcomes and using this estimate as the
+    # of deaths
+    df_in <- known_outcomes(
+      df_in = df_in,
+      delay_pmf = delay_pmf,
+      cumulative = FALSE
+    )
+  }
+  # prepare cumulative sums
+  cumulative_cases <- cumsum(df_in$cases)
+  cumulative_deaths <- cumsum(df_in$deaths)
 
-  df_cfr <- data.frame()
+  # empty list for results
+  cfr_estimate <- list()
+
   # calculating the corrected CFR rolling over all days
   if (correct_for_delays) {
-    df_cfr <- lapply(n_days, function(i) {
-      static_cfr(
-        df_in = df_in[seq_len(i), ],
-        correct_for_delays = TRUE,
-        delay_pmf = delay_pmf
-      )
-    })
+    # calculating the proportion of cases with known outcome
+    u_t <- cumsum(df_in$known_outcomes) / cumulative_cases
+
+    # calculating the maximum likelihood estimate and 95% confidence interval
+    # using the binomial likelihood function from Nishiura
+    cfr_estimate <- Map(
+      cumulative_cases, cumulative_deaths, u_t,
+      f = estimate_ccfr, poisson_threshold = poisson_threshold
+    )
   } else {
     # calculating the uncorrected CFR rolling over all days
-    df_cfr <- lapply(n_days, function(i) {
-      static_cfr(
-        df_in = df_in[seq_len(i), ],
-        correct_for_delays = FALSE
-      )
-    })
-  }
+    cfr_me <- cumulative_deaths / cumulative_cases
 
-  # bind the data together
-  df_cfr <- data.table::setDF(data.table::rbindlist(lapply(df_cfr, as.list)))
+    cfr_lims <- Map(
+      cumulative_deaths, cumulative_cases,
+      f = stats::binom.test, p = 1
+    )
+    cfr_estimate <- Map(
+      cfr_lims, cfr_me,
+      f = function(bintest, me) {
+        c(me, bintest[["conf.int"]])
+      }
+    )
+  }
+  df_cfr <- as.data.frame(
+    data.table::transpose(
+      cfr_estimate
+    ),
+    col.names = c("cfr_me", "cfr_low", "cfr_high")
+  )
+
   # attaching the dates, cases and deaths to the CFR estimates
   df_cfr <- cbind(df_cfr, df_in[, c("date", "cases", "deaths")])
 
