7834bfbb293c35980eca41f4ef0d4f6532a53efc
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2022-12-16 15:11:04 +0000

    Update data documentation

--- R/ebola1976.R
+++ R/ebola1976.R
@@ -1,6 +1,7 @@
 #' Ebola 1976 outbreak case data
 #'
 #' An example epidemic outbreak dataset for use with the `datadelay` package.
+#' This dataset comes from the first Ebola outbreak in Zaire in 1976.
 #'
 #' @format ## `ebola1976`
 #' A data frame with 73 rows and 60 columns:
@@ -9,5 +10,5 @@
 #'   \item{cases}{Number of cases reported}
 #'   \item{deaths}{Number of deaths reported}
 #' }
-#' @source WIP UNKNOWN
+#' @source \doi{10.1016/j.epidem.2014.09.003}
 "ebola1976"
