d32dce0fbb264b5d4b19e9efc4bdd30620fe26a0
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-11 13:03:42 +0100

    Add covid data as CSV, fixes #71

--- data-raw/covid_data.R
+++ data-raw/covid_data.R
@@ -1,29 +1,12 @@
 ## code to prepare `covid_data` dataset goes here
 
-library(dplyr)
-
-# get data from covidregionaldata
-covid_data <- covidregionaldata::get_national_data(
-  source = "who", verbose = FALSE
+covid_data <- read.csv(
+  system.file(
+    "extdata", "covid_data.csv",
+    package = "cfr",
+    mustWork = TRUE
+  )
 )
-
-# rename columns
-covid_data <- rename(covid_data, cases = cases_new, deaths = deaths_new)
-
-# get countries with >= 100,000 deaths over the first three
-# years of the pandemic, 2020, 2021, and 2022
-covid_data <- covid_data %>%
-  filter(date < "2023-01-01") %>%
-  group_by(iso_code) %>%
-  mutate(total_deaths = max(deaths_total)) %>%
-  filter(total_deaths > 100000 & !is.na(country)) %>%
-  ungroup()
-
-# select columns
-covid_data <- covid_data %>%
-  select(date, country, cases, deaths)
-
-# convert to data.frame
-covid_data <- as.data.frame(covid_data)
+covid_data$date <- as.Date(covid_data$date)
 
 usethis::use_data(covid_data, overwrite = TRUE)
