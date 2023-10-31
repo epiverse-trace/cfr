28ac239e0631d95a5dc525ca48c0739ecfc3985e
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-10-31 17:10:20 +0000

    Add internal fn to check fn for N req args

---
+++ R/helper_delay_density.R
@@ -0,0 +1,23 @@
+#' Check delay density functions passed to `cfr_*()`
+#'
+#' Checks whether a function has a given number of required arguments and
+#' returns a numeric of the same length as the vector of values on which it
+#' operates.
+#'
+#' @param fn A function.
+#' @param n_req_args The number of required arguments, i.e., arguments without
+#' default values.
+#'
+#' @return A logical for whether the function `fn` has the number of required
+#' arguments specified by the user.
+#' @keywords internal
+test_fn_req_args <- function(fn, n_req_args = 1) {
+  checkmate::assert_count(n_req_args, positive = TRUE)
+  checkmate::test_function(fn) &&
+    identical(
+      length(
+        Filter(is.name, formals(fn))
+      ),
+      as.integer(n_req_args)
+    )
+}
