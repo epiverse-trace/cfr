8654c24d96675cc7451501285e3c648894343b58
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-11-16 13:38:13 +0000

    Add data description and source in data-raw

--- data-raw/covid_data.R
+++ data-raw/covid_data.R
@@ -1,5 +1,15 @@
-## code to prepare `covid_data` dataset goes here
-
+## code to prepare `covid_data` dataset
+# Data adapted from the \{covidregionaldata\} package of daily cases and
+# deaths from the 19 countries with 100,000 or more deaths over the period
+# 2020-01-01 to 2022-12-31. See the **References** for the publication which
+# links to data sources made available through \{covidregionaldata\}.
+# Included as \{covidregionaldata\} is no longer on CRAN.
+# Data are provided as a `<data.frame>`.
+#
+# Citation:
+# Joseph Palmer, Katharine Sherratt, Richard Martin-Nielsen, Jonnie Bevan,
+# Hamish Gibbs, Sebastian Funk and Sam Abbott (2021). covidregionaldata:
+# Subnational data for COVID-19 epidemiology. \doi{10.21105/joss.03290}
 covid_data <- read.csv(
   system.file(
     "extdata", "covid_data.csv",
--- data-raw/ebola1976.R
+++ data-raw/ebola1976.R
@@ -1,5 +1,12 @@
-## code to prepare `ebola1976` dataset goes here
-
+## code to prepare `ebola1976` dataset
+# An example epidemic outbreak dataset for use with the `cfr` package.
+# This dataset comes from the first Ebola outbreak in Zaire in 1976 as analysed
+# in Camacho et al. (2014).
+#
+# Citation:
+# Camacho, A., Kucharski, A. J., Funk, S., Breman, J., Piot, P., &
+# Edmunds, W. J. (2014). Potential for large outbreaks of Ebola virus disease.
+# Epidemics, 9, 70–78. \doi{10.1016/j.epidem.2014.09.003}
 ebola1976 <- read.csv(
   system.file(
     "extdata", "ebola_1976.csv",
