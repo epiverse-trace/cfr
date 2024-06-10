ecfab88e16383b2a0ab7f98d1cc37e92b3a78089
pratikunterwegs <pratikgupte16@gmail.com>
2024-06-10 12:32:44 +0100

    Add release tasks fn and test

---
+++ R/dev-utils.R
@@ -0,0 +1,8 @@
+# This unexported function adds a custom item to `usethis::use_release_issue()`
+release_bullets <- function() {
+  c(
+    "Run `goodpractice::gp()`",
+    "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
+    "Check if `# nolint` comments are still needed with recent lintr releases"
+  )
+}
