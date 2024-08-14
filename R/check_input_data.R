734df58ed9102f56917b15061144409993f1d4b5
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-08-14 10:13:12 +0200

    Use internal checking function to reduce duplication

---
+++ R/check_input_data.R
@@ -0,0 +1,32 @@
+.check_input_data <- function(data) {
+
+  checkmate::assert_data_frame(
+    data,
+    min.rows = 1, min.cols = 3
+  )
+  # check that input `<data.frame>` has columns date, cases, and deaths
+  checkmate::assert_names(
+    colnames(data),
+    must.include = c("date", "cases", "deaths")
+  )
+  # check for any NAs among data
+  checkmate::assert_data_frame(
+    data[, c("date", "cases", "deaths")],
+    types = c("Date", "integerish"),
+    any.missing = FALSE,
+  )
+  # check that data$date is a date column
+  checkmate::assert_date(data$date, any.missing = FALSE, all.missing = FALSE)
+
+  # Check count types
+  checkmate::assert_integerish(data$cases, lower = 0)
+  checkmate::assert_integerish(data$deaths, lower = 0)
+
+  stopifnot(
+    "Input data must have sequential dates with none missing or duplicated" =
+      identical(unique(diff(data$date)), 1) # use numeric 1, not integer
+    # this solution works when df$date is `Date`
+    # this may need more thought for dates that are integers, POSIXct,
+    # or other units; consider the units package
+  )
+}
